1feeb210ac9a8ae95a648e8214790cc766c494c7
ElPassio <passiotti.agustin@gmail.com>
2025-11-07 11:29:21 -0300

    updated live chat

--- js/script.js
+++ js/script.js
@@ -50,7 +50,9 @@ function loadTranslations(langFile) {
             document.querySelector('.proyecto1 p').innerHTML = data.proyecto1_p;
             document.querySelector('.proyecto2 h5').innerHTML = data.proyecto2_h5;
             document.querySelector('.proyecto2 p').innerHTML = data.proyecto2_p;
-            
+            document.querySelector('.proyecto3 h5').innerHTML = data.proyecto3_h5;
+            document.querySelector('.proyecto3 p').innerHTML = data.proyecto3_p;
+
             document.querySelector('#footer h4').textContent = data.contact;
             initializeTyped(data.typed_strings);
         })
